848513e1ad60b1b20788aadba5b4f28c01a1de9b
Andrew Chaa <andrew.chaa@huddle.net>
2012-08-08 15:47:21 +0100

    compare franc with dollar

--- src/MultiCurrenciesExample/CurrenciesTest.java
+++ src/MultiCurrenciesExample/CurrenciesTest.java
@@ -24,8 +24,12 @@ public class CurrenciesTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(6).equals(new Dollar(5)));
+
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(6).equals(new Franc(5)));
+
+        assertFalse(new Franc(6).equals(new Dollar(6)));
+
     }
 
     @Test
